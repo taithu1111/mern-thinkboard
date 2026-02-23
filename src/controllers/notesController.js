8ffbdfdc7c1f0acdb2007baed7df3eb16f763a9c
thunpt <nguyenphanbaolong1402@gmail.com>
2026-02-24 00:43:52 +0700

    task3: Optimizing File & Folder Structure - extract notes controller into separate file

---
+++ src/controllers/notesController.js
@@ -0,0 +1,15 @@
+export function getAllNotes(req, res) {
+    res.status(200).send("you just fetched the notes");
+}
+
+export function createNote(req, res) {
+    res.status(200).json({ message: "note created successfully" });
+}
+
+export function updateNote(req, res) {
+    res.status(200).json({ message: "note updated successfully" });
+}
+
+export function deleteNote(req, res) {
+    res.status(200).json({ message: "note deleted successfully" });
+}
