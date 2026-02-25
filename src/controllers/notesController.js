bcd2665ecd520d7582d463faf874cc69c25aaa73
thunpt <nguyenphanbaolong1402@gmail.com>
2026-02-25 22:59:52 +0700

    feat: Implement initial backend for note management, including a Mongoose Note model, CRUD controllers, API routes, and server setup.

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,15 +1,65 @@
-export function getAllNotes(req, res) {
-    res.status(200).send("you just fetched the notes");
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find().sort({ createdAt: -1 });
+        res.status(200).json(notes);
+    } catch (error) {
+        console.log("Error in get all notes controller");
+        res.status(500).json({ message: "interval server error" });
+    }
 }
 
-export function createNote(req, res) {
-    res.status(200).json({ message: "note created successfully" });
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ message: "note not found" });
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        console.log("Error in get note by id controller", error);
+        res.status(500).json({ message: "interval server error" });
+    }
 }
 
-export function updateNote(req, res) {
-    res.status(200).json({ message: "note updated successfully" });
+export async function createNote(req, res) {
+    try {
+        const { title, content } = req.body;
+        const newNote = new Note({ title, content });
+
+        await newNote.save();
+        res.status(201).json({ message: "note created successfully" });
+    } catch (error) {
+        console.log("Error in create note controller", error);
+        res.status(500).json({ message: "interval server error" });
+    }
 }
 
-export function deleteNote(req, res) {
-    res.status(200).json({ message: "note deleted successfully" });
+export async function updateNote(req, res) {
+    try {
+        const { title, content } = req.body;
+        const updateNote = await Note.findByIdAndUpdate(req.params.id, { title, content }, { new: true });
+
+        if (!updateNote) {
+            return res.status(404).json({ message: "note not found" });
+        }
+        res.status(200).json(updateNote);
+    } catch (error) {
+        console.error("Error in update note controller", error);
+        res.status(500).json({ message: "interval server error" });
+    }
+
+}
+export async function deleteNote(req, res) {
+    try {
+        const deleteNote = await Note.findByIdAndDelete(req.params.id);
+        if (!deleteNote) {
+            return res.status(404).json({ message: "note not found" });
+        }
+        res.status(200).json({ message: "note deleted successfully", deleteNote });
+    } catch (error) {
+        console.log("Error in delete note controller", error);
+        res.status(500).json({ message: "interval server error" });
+    }
 }
