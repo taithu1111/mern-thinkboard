a1e87624a9557b6776af69b6b54aa3280fe0c4cf
thunpt <nguyenphanbaolong1402@gmail.com>
2026-02-27 11:57:15 +0700

    feat: Add Upstash-based API rate limiting middleware and integrate it into the server.

--- src/server.js
+++ src/server.js
@@ -2,21 +2,29 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
 
 //middleware
-app.use(express.json());
+app.use(express.json()); //this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+//our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method: ${req.method} and req url: ${req.url}`);
+//   next();
+// });
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server started on Port:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on Port:", PORT);
+  });
 });
 
 
