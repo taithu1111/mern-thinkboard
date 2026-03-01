9d89fc805c3f7adc91bb91d9be8a66cd078dcaf2
thunpt <nguyenphanbaolong1402@gmail.com>
2026-03-01 12:57:30 +0700

    feat: Set up initial Express server with CORS, JSON parsing, and database connection.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
@@ -13,6 +15,9 @@ const PORT = process.env.PORT || 5001;
 //middleware
 app.use(express.json()); //this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 //our simple custom middleware
 // app.use((req, res, next) => {
 //   console.log(`Req method: ${req.method} and req url: ${req.url}`);
